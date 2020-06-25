ff6cbfe0755b83d5e0498334f756988798b9cd22
Marcos <marcos.unizar@gmail.com>
2020-06-25 11:28:14 +0200

    Files restablished

--- MultiFibberOpticalTimeBUNCH_1mm_800/MultipleFibbersOpticalTimeBUNCH/test_1_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash/
-
-#cd /lhome/ific/m/marcosmr/MultipleFibbersOpticalTimeBUNCH/build
-
-cd /home/marcosmr/Escritorio/MultiFibberOpticalTimeBUNCH_1mm_800/MultipleFibbersOpticalTimeBUNCH/build
-
-./MultipleFibbers run1.mac 0 1000000
-
