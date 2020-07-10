b230492076abfea24d703e7fedf63a01dde7dc0b
Marcos <marcos.unizar@gmail.com>
2020-07-10 10:07:09 +0200

    update

---
+++ 1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/Scintillator.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/Scintillator.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/ActionInitialization.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/ActionInitialization.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/DetectorConstruction.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/DetectorConstruction.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/EventAction.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/EventAction.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/Materials.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/Materials.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/PhysListEmStandard.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/PhysListEmStandard.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/PhysListEmStandardSSM.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/PhysListEmStandardSSM.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/PhysicsList.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/PhysicsList.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/PhysicsListMessenger.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/PhysicsListMessenger.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/PrimaryGeneratorGPS.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/PrimaryGeneratorGPS.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/RunAction.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/RunAction.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/StepMax.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/StepMax.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/StepMaxMessenger.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/StepMaxMessenger.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/sipmHit.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/sipmHit.cc.o"
+  "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/src/sipmSD.cc" "/home/marcosmr/Documentos/Geant4/Tritium_Simulations/1_fibra/Scintillator/test/CMakeFiles/Scintillator.dir/src/sipmSD.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/CLHEP-2.4.1.3/../../include"
+  "/usr/local/Geant4ST/include/Geant4"
+  "../include"
+  "/usr/local/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Setup_Geant4_CMake_Root_Cosmics.sh
@@ -0,0 +1,5 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup geant4 v4_10_6_p01 -q e19:prof
+setup cmake v3_14_3
+setup root v6_18_04d -q e19:prof
+source /lhome/ific/m/marcosmr/20cm1mmFibers_without_Lead_without_VETO/cry_v1.7/setup
